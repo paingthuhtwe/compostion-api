5aba2fd81dfd7315734bb36823809d29fd8617c8
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-14 09:54:38 +0630

    search with computed properties

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,30 @@
 <template>
   <div class="home">
-    <div class="border-b-2 py-9">
-      <h1 class="text-center text-3xl font-semibold">{{ userOne.name }}</h1>
-      <h1 class="text-center text-3xl font-semibold">{{ userOne.age }}</h1>
-      <button
-        @click="change('one')"
-        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
-      >
-        Change Name
-      </button>
-      <button
-        @click="userOne.age++"
-        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
-      >
-        Increase Age
-      </button>
+    <input
+      type="text"
+      id="first_name"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 mx-auto"
+      placeholder="Search"
+      v-model="search"
+    />
+    <h1 class="text-center text-3xl font-semibold">{{ search }}</h1>
+    <div class="border-b-2 py-9" v-for="name in filterNames" :key="name">
+      <h1 class="text-center text-3xl font-semibold">{{ name }}</h1>
     </div>
-    <!-- end userOne -->
-    <div class="border-b-2 py-9">
-      <h1 class="text-center text-3xl font-semibold">{{ userTwo.name }}</h1>
-      <h1 class="text-center text-3xl font-semibold">{{ userTwo.age }}</h1>
-      <button
-        @click="change('two')"
-        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
-      >
-        Change Name
-      </button>
-      <button
-        @click="userTwo.age++"
-        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
-      >
-        Increase Age
-      </button>
-    </div>
-    <!-- end userTwo -->
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 export default {
   setup() {
-    let userOne = ref({ name: "User One", age: "20" });
-    let userTwo = reactive({ name: "User Two", age: "30" });
-    let change = (value) => {
-      if (value === "one") {
-        userOne.value.name = "User One Change";
-      }
-      if (value === "two") {
-        userTwo.name = "User Two Change";
-      }
-    };
+    let search = ref("");
+    let names = ref(["Panda", "Thet Hnin Phyu", "Paing Thu Htwe", "Htar"]);
 
-    return { userOne, userTwo, change };
+    let filterNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+    return { names, search, filterNames };
   },
 };
 </script>
