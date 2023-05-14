a102de4e085dd7f1b177028bd8c874e02801ab8b
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-14 09:34:09 +0630

    refs and reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="border-b-2 py-9">
+      <h1 class="text-center text-3xl font-semibold">{{ userOne.name }}</h1>
+      <h1 class="text-center text-3xl font-semibold">{{ userOne.age }}</h1>
+      <button
+        @click="change('one')"
+        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
+      >
+        Change Name
+      </button>
+      <button
+        @click="userOne.age++"
+        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
+      >
+        Increase Age
+      </button>
+    </div>
+    <!-- end userOne -->
+    <div class="border-b-2 py-9">
+      <h1 class="text-center text-3xl font-semibold">{{ userTwo.name }}</h1>
+      <h1 class="text-center text-3xl font-semibold">{{ userTwo.age }}</h1>
+      <button
+        @click="change('two')"
+        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
+      >
+        Change Name
+      </button>
+      <button
+        @click="userTwo.age++"
+        class="px-3 py-1 bg-slate-700 hover:bg-slate-800 rounded-md text-slate-100 mt-9 m-3"
+      >
+        Increase Age
+      </button>
+    </div>
+    <!-- end userTwo -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { reactive, ref } from "vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    let userOne = ref({ name: "User One", age: "20" });
+    let userTwo = reactive({ name: "User Two", age: "30" });
+    let change = (value) => {
+      if (value === "one") {
+        userOne.value.name = "User One Change";
+      }
+      if (value === "two") {
+        userTwo.name = "User Two Change";
+      }
+    };
+
+    return { userOne, userTwo, change };
+  },
+};
 </script>
