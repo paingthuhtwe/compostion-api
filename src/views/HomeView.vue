740649b84a6cb0193fcc7d1b748eda0dcdf767ed
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-15 21:50:51 +0630

    create TagCloud Component UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="home md:w-[790px] lg:w-[1200px] mx-auto" v-if="posts.length > 0">
-    <PostList :posts="posts" :error="error"></PostList>
+  <div
+    class="home w-full md:w-[790px] lg:w-[1200px] mx-auto"
+    v-if="posts.length > 0"
+  >
+    <div class="grid grid-cols-12">
+      <div class="lg:col-span-8 md:col-span-6 col-span-12">
+        <h1 class="text-2xl font-semibold text-slate-800 my-4 text-left">
+          Post Lists
+        </h1>
+        <PostList :posts="posts" :error="error"></PostList>
+      </div>
+      <div class="lg:col-span-4 md:col-span-6 col-span-12">
+        <h1 class="text-2xl font-semibold text-slate-800 my-4">Tag Cloud</h1>
+        <TagCloud></TagCloud>
+      </div>
+    </div>
   </div>
   <div v-else>
     <LoadingPage></LoadingPage>
@@ -8,11 +22,13 @@
 </template>
 
 <script>
+import TagCloud from "../components/TagCloud";
 import LoadingPage from "../components/LoadingPage";
 import PostList from "./PostList";
 import getPosts from "../composables/getPosts";
 export default {
   components: {
+    TagCloud,
     LoadingPage,
     PostList,
   },
--- src/views/TagPost.vue
+++ src/views/TagPost.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="w-full md:w-[790px] lg:w-[1200px] mx-auto">
-    <div
-      class="home md:w-[790px] lg:w-[1200px] mx-auto"
-      v-if="posts.length > 0"
-    >
-      <PostList :posts="filterPosts" :error="error"></PostList>
+  <div
+    class="home w-full md:w-[790px] lg:w-[1200px] mx-auto"
+    v-if="posts.length > 0"
+  >
+    <div class="grid grid-cols-12">
+      <div class="lg:col-span-8 md:col-span-6 col-span-12">
+        <h1 class="text-2xl font-semibold text-slate-800 my-4 text-left">
+          Post Lists
+        </h1>
+        <PostList :posts="filterPosts" :error="error"></PostList>
+      </div>
+      <div class="lg:col-span-4 md:col-span-6 col-span-12">
+        <h1 class="text-2xl font-semibold text-slate-800 my-4">Tag Cloud</h1>
+        <TagCloud></TagCloud>
+      </div>
     </div>
-    <div v-else>
-      <LoadingPage></LoadingPage>
-    </div>
+  </div>
+  <div v-else>
+    <LoadingPage></LoadingPage>
   </div>
 </template>
 
